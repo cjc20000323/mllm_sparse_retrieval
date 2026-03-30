1eafdda6a1542f8daffaaca67e5ffa4588d6f89f
cjc20000323 <62797731+cjc20000323@users.noreply.github.com>
2026-03-30 23:51:05 +0800

    20260330

--- src/reranker.py
+++ src/reranker.py
@@ -1272,7 +1272,7 @@ class Reranker:
                             inputs = self.processor(images=image_shard, text=text_input, return_tensors="pt").to(
                                 self.model.device)
                             if dist.get_rank() == 0:
-                                print(inputs.shape)
+                                print(inputs['input_ids'].shape)
                             max_inputs_sum = inputs['input_ids'].shape[1]
                             labels = [self.processor(text=text, return_tensors="pt")[
                                           'input_ids'].squeeze().tolist()] * len(
