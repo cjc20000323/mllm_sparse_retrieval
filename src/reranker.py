61ee493f813f8b77e56cc5a10eb329d098d07492
cjc20000323 <62797731+cjc20000323@users.noreply.github.com>
2026-03-30 23:30:46 +0800

    20260330 flops

--- src/reranker.py
+++ src/reranker.py
@@ -1175,8 +1175,6 @@ class Reranker:
                             image_shard = [raw_image] * len(text_shard)
                             inputs = self.processor(images=image_shard, text=text_input, return_tensors="pt").to(
                                 self.model.device)
-                            if dist.get_rank() == 0:
-                                print(inputs.shape)
                             max_inputs_sum = inputs['input_ids'].shape[1]
                             data_sum += rerank_batch_size
                             token_sum += max_inputs_sum
@@ -1273,6 +1271,8 @@ class Reranker:
                             text_input = [rerank_prompt_template + text] * len(image_shard)
                             inputs = self.processor(images=image_shard, text=text_input, return_tensors="pt").to(
                                 self.model.device)
+                            if dist.get_rank() == 0:
+                                print(inputs.shape)
                             max_inputs_sum = inputs['input_ids'].shape[1]
                             labels = [self.processor(text=text, return_tensors="pt")[
                                           'input_ids'].squeeze().tolist()] * len(
