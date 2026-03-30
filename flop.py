159e201455839868f9dab049d0c0555d122bcc7c
cjc20000323 <62797731+cjc20000323@users.noreply.github.com>
2026-03-30 22:18:43 +0800

    20260330 flops

--- flop.py
+++ flop.py
@@ -44,6 +44,16 @@ ax.grid(True)
 
 ax.legend(['CLIP', 'three pipelines'], loc='lower right', fontsize=25)
 # plt.tight_layout(rect=[0, 0, 1, 0.95])  # 调整整体布局以防止重叠
+plt.text(0.3, 0.3,  # 坐标位置 (图形坐标，0-1之间)
+         r'$f_2(x)=1436x$',  # LaTeX公式
+         transform=plt.gca().transAxes,  # 使用坐标轴坐标
+         fontsize=20,
+         bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.8))
+plt.text(0, 0.6,  # 坐标位置 (图形坐标，0-1之间)
+         r'$f_1(x)=300x+4096\times 2\times200+2\times5\times8B\times2000$',  # LaTeX公式
+         transform=plt.gca().transAxes,  # 使用坐标轴坐标
+         fontsize=18,
+         bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.8))
 plt.savefig('flop_rerank.pdf', format='pdf', bbox_inches='tight', pad_inches=0.05)
 
 x = np.linspace(0, 9000)
